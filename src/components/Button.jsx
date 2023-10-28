8b85e12a97e11c11041544534b5321bfbf11461d
ayoquiroga <ayo.quiroga@gmail.com>
2023-10-28 08:54:28 -0300

    apipokemoncard initial

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import '../sass/Button.scss'
+
+const Button = ({icon, handleClick}) => {
+
+  return (
+    <div className='button__box'>
+        <button className='button' onClick={handleClick}>{icon}</button>
+        <div className='button__shadow'></div>
+    </div>
+   )
+}
+
+export {Button}
